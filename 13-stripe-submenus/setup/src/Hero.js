6c4e8b5f0a7200e4887c3aceb5bb6e278d5683d9
Jinkoo <66558109+jinkoo86@users.noreply.github.com>
2021-03-20 15:01:22 +0900

    Submenu

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import phoneImg from './images/phone.svg'
+import { useGlobalContext } from './context'
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  const { closeSubmenu } = useGlobalContext()
+
+  return (
+    <section class='hero'>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>Payments infracstructure for the internet</h1>
+          <p>
+            Millions of compaines of all sizes-from startups to Fortune 500s-use
+            Stripe's software and APIs to accept paymnets, send payouts, and
+            manage their bussiness online.
+          </p>
+          <button className='btn'>Start now</button>
+        </article>
+        <article className='hero-images'>
+          <img src={phoneImg} alt='phone-img' alt='phone' />
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,43 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, createContext } from 'react'
 import sublinks from './data'
+
+const AppContext = createContext()
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true)
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true)
+  }
+  const closeSidebar = () => {
+    setIsSidebarOpen(false)
+  }
+  const openSubmenu = () => {
+    setIsSubmenuOpen(true)
+  }
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false)
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSubmenuOpen,
+        isSidebarOpen,
+        openSubmenu,
+        openSidebar,
+        closeSubmenu,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
